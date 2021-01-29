04d82425da32eccac7cbf001e13406e4b558d075
vanshsikka <vsikka2@illinois.edu>
2021-01-28 22:13:49 -0600

    Added Unreachable Statement and No Winner Evaluations to the Evaluation method.

--- src/com/example/TicTacToeBoard.java
+++ src/com/example/TicTacToeBoard.java
@@ -1,16 +1,19 @@
 package com.example;
 
+import java.util.Locale;
+
 /**
  * Takes in and evaluates a string representing a tic tac toe board.
  */
 public class TicTacToeBoard {
-
+  private String ticTacToeBoard;
+  private final int BOARD_SIZE = 9;
   /**
    * This method should load a string into your TicTacToeBoard class.
    * @param board The string representing the board
    */
   public TicTacToeBoard(String board) {
-
+    ticTacToeBoard = board;
   }
 
   /**
@@ -18,6 +21,24 @@ public class TicTacToeBoard {
    * @return an enum value corresponding to the board evaluation
    */
   public Evaluation evaluate() {
+    char[] charTicTacToeBoard = ticTacToeBoard.toUpperCase().toCharArray();
+    if(ticTacToeBoard.length() != BOARD_SIZE) {
+      return Evaluation.UnreachableState;
+    }
+    int countX = 0;
+    int countO = 0;
+    for (char c: charTicTacToeBoard) {
+      if (c == 'X') {
+        countX++;
+      } else if (c == 'O') {
+        countO++;
+      }
+    }
+    if (Math.abs(countX - countO) > 1) {
+      return Evaluation.UnreachableState;
+    } else if (countX < 3 || countO < 3) {
+      return Evaluation.NoWinner;
+    }
     return Evaluation.UnreachableState;
   }
 }
--- test/com/example/TicTacToeBoardTest.java
+++ test/com/example/TicTacToeBoardTest.java
@@ -9,4 +9,9 @@ public class TicTacToeBoardTest {
     TicTacToeBoard board = new TicTacToeBoard("O...X.X..");
     assertEquals(Evaluation.NoWinner, board.evaluate());
   }
+  @Test
+  public void testValidBoardUnreachableState(){
+    TicTacToeBoard board = new TicTacToeBoard("X..X..X.O");
+    assertEquals(Evaluation.UnreachableState, board.evaluate());
+  }
 }
